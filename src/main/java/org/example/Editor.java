cc4d90e60be16ca627e4b379ed350f9031166055
dannnytt <danielternovski@gmail.com>
2026-02-19 01:34:11 +0700

    Added editor class and tests for him

---
+++ src/main/java/org/example/Editor.java
@@ -0,0 +1,93 @@
+package org.example;
+
+import java.util.Objects;
+
+public class Editor {
+
+    private String number;
+
+    private static final String ZERO = "0";
+    private static final String DELIM = ".";
+
+    public enum Commands {
+        add0, add1, add2, add3,
+        add4, add5, add6, add7,
+        add8, add9, addA, addB,
+        addC, addD, addE, addF,
+        addDot, bs, clear, exec
+    }
+
+    public Editor() { number = ""; }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void addDigit(int digit) {
+
+        if (digit < 0 || digit > 16)
+            throw new IllegalArgumentException("Параметр digit должен быть [0, 16]");
+
+        if (Objects.equals(number, ZERO))
+            number = String.valueOf(ADTConvert10P.digitToChar(digit));
+        else number += String.valueOf(ADTConvert10P.digitToChar(digit));
+    }
+
+    public int acc() {
+
+        if (number.contains(DELIM)) {
+            String[] parts = number.split("\\.");
+            return parts[1].length();
+        }
+
+        return 0;
+    }
+
+    public void addZero() {
+        number += ZERO;
+    }
+
+    public void addDelim() {
+
+        if (number.isEmpty())
+            number += ZERO;
+
+        if(!number.contains(DELIM))
+            number += DELIM;
+    }
+
+    public void bs() {
+
+        if (number.length() > 1)
+            number = number.substring(0, number.length() - 1);
+        else number = ZERO;
+    }
+
+    public void clear() {
+        number = "";
+    }
+
+    public void doEdit(Commands command) {
+        switch (command) {
+            case Commands.add0 -> addZero();
+            case Commands.add1 -> addDigit(1);
+            case Commands.add2 -> addDigit(2);
+            case Commands.add3 -> addDigit(3);
+            case Commands.add4 -> addDigit(4);
+            case Commands.add5 -> addDigit(5);
+            case Commands.add6 -> addDigit(6);
+            case Commands.add7 -> addDigit(7);
+            case Commands.add8 -> addDigit(8);
+            case Commands.add9 -> addDigit(9);
+            case Commands.addA -> addDigit(10);
+            case Commands.addB -> addDigit(11);
+            case Commands.addC -> addDigit(12);
+            case Commands.addD -> addDigit(13);
+            case Commands.addE -> addDigit(14);
+            case Commands.addF -> addDigit(15);
+            case Commands.addDot -> addDelim();
+            case Commands.bs -> bs();
+            case Commands.clear -> clear();
+        }
+    }
+}
---
+++ src/test/java/org/example/EditorTest.java
@@ -0,0 +1,220 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EditorTest {
+
+    @Test void testAddDigit1() {
+        Editor editor = new Editor();
+        String expect = "";
+        String actual = editor.getNumber();
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test void testAddDigit2() {
+        Editor editor = new Editor();
+        editor.addDigit(0);
+        editor.addDigit(0);
+        editor.addDigit(0);
+        String expect = "0";
+        String actual = editor.getNumber();
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test void testAddDigit3() {
+        Editor editor = new Editor();
+        editor.addDigit(0);
+        editor.addDelim();
+        editor.addDigit(0);
+        editor.addDigit(0);
+        editor.addDigit(0);
+        String expect = "0.000";
+        String actual = editor.getNumber();
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test void testAddDigit4() {
+        Editor editor = new Editor();
+        editor.addDigit(15);
+        editor.addDigit(12);
+        editor.addDigit(1);
+        editor.addDelim();
+        editor.addDigit(1);
+        editor.addDigit(9);
+        String expect = "FC1.19";
+        String actual = editor.getNumber();
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test void testAddDigit5() {
+        Editor editor = new Editor();
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                ()-> editor.addDigit(17)
+        );
+    }
+
+    @Test void testAddDigit6() {
+        Editor editor = new Editor();
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                ()-> editor.addDigit(-1)
+        );
+    }
+
+    @Test void testAcc1() {
+        Editor editor = new Editor();
+        editor.addDelim();
+        editor.addDigit(1);
+        editor.addDigit(2);
+        editor.addDigit(3);
+        int expect = 3;
+        int actual = editor.acc();
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test void testAcc2() {
+        Editor editor = new Editor();
+        editor.addDigit(15);
+        editor.addDigit(12);
+        editor.addDigit(1);
+        editor.addDelim();
+        editor.addDigit(1);
+        editor.addDigit(9);
+        int expect = 2;
+        int actual = editor.acc();
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test void testAcc3() {
+        Editor editor = new Editor();
+        editor.addDigit(5);
+        int expect = 0;
+        int actual = editor.acc();
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test void testAddZero() {
+        Editor editor = new Editor();
+        editor.addDigit(1);
+        editor.addZero();
+        String expect = "10";
+        String actual = editor.getNumber();
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test void testAddDelim1() {
+        Editor editor = new Editor();
+        editor.addDelim();
+        String expect = "0.";
+        String actual = editor.getNumber();
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test void testAddDelim2() {
+        Editor editor = new Editor();
+        editor.addDelim();
+        editor.addDelim();
+        String expect = "0.";
+        String actual = editor.getNumber();
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test void testAddDelim3() {
+        Editor editor = new Editor();
+        editor.addDigit(15);
+        editor.addDigit(15);
+        editor.addDigit(15);
+        editor.addDelim();
+        editor.addDelim();
+        editor.addDelim();
+        editor.addDigit(15);
+        editor.addDigit(15);
+        editor.addDigit(15);
+        editor.addDelim();
+        editor.addDelim();
+        editor.addDelim();
+        String expect = "FFF.FFF";
+        String actual = editor.getNumber();
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test void testAddDelim4() {
+        Editor editor = new Editor();
+        editor.addDigit(0);
+        editor.addDelim();
+        editor.addDelim();
+        editor.addDelim();
+        editor.addDigit(0);
+        editor.addDelim();
+        editor.addDelim();
+        editor.addDelim();
+        String expect = "0.0";
+        String actual = editor.getNumber();
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test void testBs1() {
+        Editor editor = new Editor();
+        editor.bs();
+        editor.bs();
+        String expect = "0";
+        String actual = editor.getNumber();
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test void testBs2() {
+        Editor editor = new Editor();
+        editor.bs();
+        editor.addDigit(1);
+        editor.addDigit(2);
+        editor.bs();
+        String expect = "1";
+        String actual = editor.getNumber();
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test void testBs3() {
+        Editor editor = new Editor();
+        editor.addDigit(3);
+        editor.addDigit(3);
+        editor.addDigit(3);
+        editor.addDelim();
+        editor.bs();
+        String expect = "333";
+        String actual = editor.getNumber();
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test void testBs4() {
+        Editor editor = new Editor();
+        editor.addDigit(3);
+        editor.addDigit(3);
+        editor.addDigit(3);
+        editor.addDelim();
+        editor.addDigit(3);
+        editor.addDigit(3);
+        editor.addDigit(3);
+        editor.bs();
+        editor.bs();
+        String expect = "333.3";
+        String actual = editor.getNumber();
+        Assertions.assertEquals(expect, actual);
+    }
+
+    @Test void testClear() {
+        Editor editor = new Editor();
+        editor.addDigit(3);
+        editor.addDigit(3);
+        editor.addDigit(3);
+        editor.addDelim();
+        editor.addDigit(3);
+        editor.addDigit(3);
+        editor.addDigit(3);
+        editor.clear();
+        String expect = "";
+        String actual = editor.getNumber();
+        Assertions.assertEquals(expect, actual);
+    }
+}
